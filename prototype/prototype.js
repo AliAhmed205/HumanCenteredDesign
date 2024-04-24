431d258832838ff615b29d753e71737aa2e87238
AliAhmed205 <Ali_lazuli@outlook.com>
2024-04-25 00:51:23 +0200

    added Audio elements to prototype

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,4 +1,10 @@
 const removeBtn1 = document.querySelector('.addedClothing1')
+const kerst = document.querySelector('.kerst')
+const halloween = document.querySelector('.halloween')
+const classy = document.querySelector('.classy')
+const glitter = document.querySelector('.glitter')
+const helder = document.querySelector('.helder')
+const finishedBtn = document.getElementById('finish')
 
 if(removeBtn1){
 
@@ -39,3 +45,72 @@ if(removeBtn3false){
   })
 }
 
+if (glitter) {
+  glitter.addEventListener("focus", function () {
+    setTimeout(() => {
+      const audio = new Audio();
+      audio.src = "../audio/glitters.mp3";
+      audio.play();
+      audio.loop = false;
+      audio.volume = 1;
+    }, 1000);
+  });
+}
+
+if (classy) {
+  classy.addEventListener("focus", function () {
+    setTimeout(() => {
+      const audio = new Audio();
+      audio.src = "../audio/noir.mp3";
+      audio.play();
+      audio.loop = false;
+      audio.volume = 1;
+    }, 1000);
+  });
+}
+
+if (kerst) {
+  kerst.addEventListener("focus", function () {
+    setTimeout(() => {
+      const audio = new Audio();
+      audio.src = "../audio/kerst.mp3";
+      audio.play();
+      audio.loop = false;
+      audio.volume = .6;
+    }, 1000);
+  });
+}
+
+if (halloween) {
+  halloween.addEventListener("focus", function () {
+    setTimeout(() => {
+      const audio = new Audio();
+      audio.src = "../audio/halloween.mp3";
+      audio.play();
+      audio.loop = false;
+      audio.volume = 1;
+    }, 1000);
+  });
+}
+if (helder) {
+  helder.addEventListener("focus", function () {
+    setTimeout(() => {
+      const audio = new Audio();
+      audio.src = "../audio/helder.mp3";
+      audio.play();
+      audio.loop = false;
+      audio.volume = 1;
+    }, 1000);
+  });
+}
+
+if (finishedBtn){
+    finishedBtn.addEventListener('click', function (){
+    console.log('clicked!')
+    const audio = new Audio()
+    audio.src = "../audio/completion.mp3"
+    audio.play()
+    audio.loop = false
+    audio.volume = 1;
+  })
+}
